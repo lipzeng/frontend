579e026a0e1c1fba75dbf9b80d0200b74ec4282f
lipzeng <berdinazzijf@gmail.com>
2020-09-07 16:06:16 -0300

    Update condicionaisIf.js

--- condicionaisIf.js
+++ condicionaisIf.js
@@ -4,11 +4,11 @@ var prova1 = 15;
 var prova2 = 20;
 var prova3 = 44;
 
-notas = (prova1 + prova2 + prova3) / 2;
+var notas = (prova1 + prova2 + prova3) / 2;
 
-aprovacao = notas >= 60;
-recuperacao = notas <= 59 && notas >= 40;
-reprovacao = 39;
+var aprovacao = notas >= 60;
+var recuperacao = notas <= 59 && notas >= 40;
+var reprovacao = 39;
 
 if (aprovacao > recuperacao) {
   console.log("aprovado com: ", notas);
