9572579f5bd8de4228eed523fb55a0b0fb67ce57
lipzeng <berdinazzijf@gmail.com>
2020-09-09 15:57:42 -0300

    Update condicionaisIf.js

--- condicionaisIf.js
+++ condicionaisIf.js
@@ -1,21 +1,19 @@
 //Estudos - Condicionais if
 
 var prova1 = 15;
-var prova2 = 20;
-var prova3 = 44;
+var prova2 = 50;
+var prova3 = 53;
 
 var notas = (prova1 + prova2 + prova3) / 2;
 
 var aprovacao = notas >= 60;
 var recuperacao = notas <= 59 && notas >= 40;
-var reprovacao = 39;
+var reprovacao = notas <= 39; // passei para boolean
 
 if (aprovacao > recuperacao) {
   console.log("aprovado com: ", notas);
-} 
-else if (recuperacao || reprovacao && recuperacao > reprovacao) {
+} else if (recuperacao > reprovacao) { // agora não preciso do ||
   console.log("recuperacao com: ", notas);
-} 
-else {
+} else {
   console.log("reprovado com: ", notas);
 }
